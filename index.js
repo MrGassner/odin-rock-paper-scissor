4707132cd7ac27da587557ef1a86f5b81ffd4b48
MrGassner <110873341+MrGassner@users.noreply.github.com>
2023-01-20 11:26:11 -0300

    jokenpo revision

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const choices = ['rock', 'paper', 'scissor'];
 
 function getComputerChoice (choices) {
     return choices[Math.floor(Math.random() * choices.length)];
-}
+};
+
 
 function getPlayerChoice () {
-    return prompt('What is your choice?: ').toLowerCase()
-}
+    return prompt('What is your choice?: ').toLowerCase();
+};
+
 
 function playRound(playerSelection, computerSelection) {
 
@@ -18,17 +20,18 @@ function playRound(playerSelection, computerSelection) {
     console.log(computerSelection)
 
     if (playerSelection === computerSelection) {
-        return 'tie'
+        return 'tie';
     } else if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        return 'won'
+        return 'won';
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return 'won'
+        return 'won';
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        return 'won'
+        return 'won';
     } else {
-        return 'lose'
-    }
-}
+        return 'lose';
+    };
+};
+
 
 let i = 0;
 let playerCount = 0;
@@ -39,13 +42,13 @@ while (playerCount < 5 && computerCount < 5) {
     let roundPlayed = playRound(getPlayerChoice(), getComputerChoice(choices));
 
     if (roundPlayed === 'won') {
-        playerCount += 1
+        playerCount += 1;
         i += 1;
     } else if (roundPlayed === 'lose') {
-        computerCount += 1
+        computerCount += 1;
         i += 1;
-    }
-}
+    };
+};
 
 console.log(`player counter: ${playerCount}`)
 console.log(`computer counter: ${computerCount}`)
@@ -54,4 +57,4 @@ if (playerCount < computerCount) {
     console.log("Computer Won")
 } else {
     console.log("You Won")
-}
+};
